0718abd81d9bfa5ca64be7235f312f282ed979ce
bntnam <bntnam.app@gmail.com>
2021-03-02 21:33:16 +0700

    [Setup] Added proxy server for deployment

--- src/components/weatherForecast/Weather/style.js
+++ src/components/weatherForecast/Weather/style.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
+import landscapeImg from '../../../assets/landscape.jpg';
 import { media } from '../../../shared';
 
 export const Wrapper = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: url('./images/landscape.jpg');
+  background-image: url(${landscapeImg});
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
